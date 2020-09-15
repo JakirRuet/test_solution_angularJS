2a3346f8d299e029c7d562e53411b40ceb75f0b3
Jakir Hossain <39633309+JakirRuet@users.noreply.github.com>
2020-09-15 22:52:10 +0600

    Add files via upload

---
+++ mod1_solutions/app.js
@@ -0,0 +1,19 @@
+(function (){
+'use strict';
+
+angular.module('LunchCheck',[])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope){
+  $scope.LunchMenu = "";
+  var dishes=[];
+  $scope.Checklist = function(){
+    dishes = $scope.LunchMenu.split(",");
+    if(dishes.length > 5){
+      document.getElementById('msg').innerHTML = dishes.length + " dishes are listed that's too much.";
+    }
+  };
+}
+})();
